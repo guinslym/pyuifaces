998ee77c92da94aaa64e76b4c51e0b0cf5847aec
Guinsly Mondesir <guinslym @gmail.com>
2015-01-03 04:24:32 -0500

    starting

--- pyuifaces/pyuifaces.py
+++ pyuifaces/pyuifaces.py
@@ -5,3 +5,31 @@ import urllib2
 
 
 class PyUIFaces(object):
+
+		@classmethod
+		def face(self):
+				pass
+
+		def faces(self):
+				return 'hello world'
+
+		def man(self):
+				pass
+
+		def woman(self):
+				pass
+
+		def sex(self):
+				pass
+
+		def local_random(self):
+				pass
+
+		def retrieve_a_link(self):
+				pass
+
+		def width(self):
+				pass
+
+foo = PyUIFaces()
+print(foo.faces())
