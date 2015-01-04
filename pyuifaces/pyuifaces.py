67515bbcec59a58fc3684e7b33b1b9f11fd37296
Guinsly Mondesir <guinslym @gmail.com>
2015-01-04 06:00:35 -0500

    Adding methods

--- pyuifaces/pyuifaces.py
+++ pyuifaces/pyuifaces.py
@@ -1,35 +1,55 @@
 import json
-import request
-import string
-import urllib2
+import requests
+import random
 
+MEN = ["brad_frost", "csswizardry", "guiiipontes", "kurafire", "tomaslau", "cemshid", "dancounsell",
+"abecherian", "mlane", "peterme", "suprb", "dingyi", "shalt0ni", "vista", "arminophen",
+"kevin_granger", "rssems", "adhamdannaway", "nexy_dre", "teleject", "9lessons", "dakshbhagya",
+"_shahedk", "itsjonq", "joshhemsley", "enda", "leemunroe", "vladarbatov", "c_southam", "felipenogs",
+"kerem", "ManikRathee", "chadengle", "peterlandt", "jaredfitch", "mattchevy", "soffes", "sindresorhus",
+"motherfuton", "mrjohnwalker", "putorti", "teclaro", "andrewaashen", "aaroni", "boheme", "dustinlamont"
+]
+
+format_image = {
+'bigger' :"https://s3.amazonaws.com/uifaces/faces/twitter/powerpointsuper/73.jpg", 
+'normal' :"https://s3.amazonaws.com/uifaces/faces/twitter/powerpointsuper/48.jpg", 
+'epic' :"https://s3.amazonaws.com/uifaces/faces/twitter/powerpointsuper/128.jpg", 
+'mini' :"https://s3.amazonaws.com/uifaces/faces/twitter/powerpointsuper/24.jpg"
+}
+
+size = {'normal': '/48.jpg', 'bigger': '/73.jpg', 'mini': '/24.jpg', 'epic': '/128.jpg'}
 
 class PyUIFaces(object):
 
-		@classmethod
-		def face(self):
-				pass
+		def __init__(self, network=False, username='random', image_size="epic", link=""):
+				self._network = network
+				self._username = username
+				self._image_size   = size[image_size]
+				self._link = "http://uifaces.com/api/v1/user/"
 
-		def faces(self):
-				return 'hello world'
+		def face(self):
+				if self._network == False and self._username == 'random': #return random.choice(MEN)
+						#select a user from USERNAME[] and make up a link
+						return self._link + random.choice(MEN) + self._image_size#local_random(image_size)
+				#retrieve a specific link
+				else:
+						try:
+								resp = requests.get(url="http://uifaces.com/api/v1/random")
+						except requests.exceptions.ConnectionError, e:
+								return self._link + random.choice(MEN) + self._image_size
+
+						if resp.ok:
+								data = json.loads(resp.text)
+								return self._link  + data['username'] + self._image_size
+						else:
+								return self._link + random.choice(MEN) + self._image_size
 
 		def man(self):
-				pass
+				return self._link + random.choice(MEN) + self._image_size
 
 		def woman(self):
-				pass
+				return self._link + random.choice(MEN) + self._image_size
 
-		def sex(self):
-				pass
+foo = PyUIFaces(True, 'random', 'normal')
+print(foo.face())
 
-		def local_random(self):
-				pass
-
-		def retrieve_a_link(self):
-				pass
-
-		def width(self):
-				pass
-
-foo = PyUIFaces()
-print(foo.faces())
